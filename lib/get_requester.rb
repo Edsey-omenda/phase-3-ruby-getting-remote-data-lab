a866f08ccdaaffeafe353805c7bb3c122d00e19c
Edsey omenda <omendajames0317@gmail.com>
2022-08-27 23:59:11 +0300

    completed

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,31 @@
 # Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+
+    # URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+    attr_accessor  :url
+
+    def initialize(url)
+        @url = url
+    end
+
+    def get_response_body
+        uri = URI.parse(self.url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def  parse_json
+        JSON.parse(self.get_response_body)
+        # programs.collect  do |program|
+        #     program["name"]
+        # end
+    end
+end
+
+
+# programs = GetRequester.new.get_response_body
+# puts programs
